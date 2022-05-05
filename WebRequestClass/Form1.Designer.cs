29a1617d59078706231dbc91157563556f193c32
Gerardo <gerazmonsivais@gmail.com>
2022-05-04 21:05:17 -0500

    Initial Commit

---
+++ WebRequestClass/Form1.Designer.cs
@@ -0,0 +1,101 @@
+﻿namespace WebRequestClass
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.top = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.cancion = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.artista = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.label1 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.top,
+            this.cancion,
+            this.artista});
+            this.dataGridView1.Location = new System.Drawing.Point(12, 30);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(345, 391);
+            this.dataGridView1.TabIndex = 2;
+            // 
+            // top
+            // 
+            this.top.HeaderText = "Top(#)";
+            this.top.Name = "top";
+            // 
+            // cancion
+            // 
+            this.cancion.HeaderText = "Nombre Canción";
+            this.cancion.Name = "cancion";
+            // 
+            // artista
+            // 
+            this.artista.HeaderText = "Artista";
+            this.artista.Name = "artista";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Times New Roman", 11.25F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(12, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(98, 18);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Top 50 Spotify";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(374, 431);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.dataGridView1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn top;
+        private System.Windows.Forms.DataGridViewTextBoxColumn cancion;
+        private System.Windows.Forms.DataGridViewTextBoxColumn artista;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ WebRequestClass/Form1.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using HtmlAgilityPack;
+using System.IO;
+using System.Net;
+
+namespace WebRequestClass
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+
+            //se realiza la peticion a la pagina web
+            var request = (HttpWebRequest)WebRequest.Create("https://spotifycharts.com/viral/mx/daily/latest");
+
+            //Aqui simulamos que se esta realizando la peticion mediante un navegador web
+            request.UserAgent = "Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/535.2 (KHTML, like Gecko) Chrome/15.0.874.121 Safari/535.2";
+            
+            //Obtenemos la respuesta
+            HttpWebResponse response = (HttpWebResponse)request.GetResponse();
+            
+            // se obtiene el html de la pagina
+            Stream dataStream = response.GetResponseStream();
+
+            // creamos un lector
+            StreamReader reader = new StreamReader(dataStream);
+
+            // se lee el contenido html
+            string responseFromServer = reader.ReadToEnd();
+
+            int inicioBusq = 0;
+            while (true)
+            {
+                int i = responseFromServer.IndexOf("class=\"chart-table-position\"", inicioBusq);
+                if (i == -1) break;
+                int inicio = responseFromServer.IndexOf(">", i) + 1;
+                int final = responseFromServer.IndexOf("<", inicio);
+                string canciones = responseFromServer.Substring(inicio, final - inicio);
+                int i2 = responseFromServer.IndexOf("class=\"chart-table-track\"", inicioBusq);
+                int inicio2 = responseFromServer.IndexOf("strong", i2) + 7;
+                int final2 = responseFromServer.IndexOf("<", inicio2);
+                string canciones2 = responseFromServer.Substring(inicio2, final2 - inicio2);
+
+                int i3 = responseFromServer.IndexOf("class=\"chart-table-track\"", inicioBusq);
+                int inicio3 = responseFromServer.IndexOf("span", i3) + 7;
+                int final3 = responseFromServer.IndexOf("<", inicio3);
+                string canciones3 = responseFromServer.Substring(inicio3, final3 - inicio3);
+
+                inicioBusq = final;
+                dataGridView1.Rows.Add(canciones,canciones2,canciones3);
+                //Console.WriteLine(canciones + " " + canciones2 + " " + canciones3);
+            }
+            //se cierran los archivos
+            reader.Close();
+            dataStream.Close();
+            response.Close();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
